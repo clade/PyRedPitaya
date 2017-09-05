70c1a06a6c9d671cf86b094d2641a8b478d2b8eb
clade <pierre.clade@spectro.jussieu.fr>
2017-09-05 17:04:19 +0200

    Add 2to3 to the setup.py

--- setup.py
+++ setup.py
@@ -150,5 +150,6 @@ setup(name='PyRedPitaya',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'],
+      use_2to3=True,
       keywords=['redpitaya', 'FPGA', 'zynq'],
      )
